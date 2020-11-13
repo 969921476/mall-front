84655f9785b0b54093aa2a94ae7c7a37c4d0e638
chenjunnan <m18460365733_1@163.com>
2020-11-13 15:50:50 +0800

    init

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>测试</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
